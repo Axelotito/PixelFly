3a6c61e2982299ee66d0f3941e9753a5a7e44de5
L. Axel <axelcruz40@aragon.unam.mx>
2026-09-22 13:16:03 -0600

    Ultimo Video, No Menu

---
+++ Assets/Assets/Script/ColisionadorTubos.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class ColisionadorTubos : MonoBehaviour
+{
+    private GameObject[] grupoTubos;
+    private float distancia = 5.0f;
+    private float ultimaXTubo;
+    private float yminTubo = -3.0f; 
+    private float ymaxTubo = 3.3f;
+
+    void Awake()
+    {
+        grupoTubos = GameObject.FindGameObjectsWithTag("tuboGrupo");
+        for (int i =0; i < grupoTubos.Length; i++)
+        {
+            Vector3 temp = grupoTubos[i].transform.position;
+            temp.y = Random.Range(yminTubo, ymaxTubo);
+            grupoTubos[i].transform.position = temp;
+        }
+
+        ultimaXTubo = grupoTubos[0].transform.position.x;
+
+        for (int i = 1; i < grupoTubos.Length; i++)
+        {
+            if (ultimaXTubo < grupoTubos[i].transform.position.x)
+            {
+                ultimaXTubo = grupoTubos[i].transform.position.x;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "tuboGrupo")
+        {
+            Vector3 temp = obj.transform.position;
+            temp.x = ultimaXTubo + distancia;
+            temp.y = Random.Range(yminTubo, ymaxTubo);
+            obj.transform.position = temp;
+            ultimaXTubo = temp.x;
+ 
+        }
+    }
+}
--- Assets/Assets/Script/Player.cs
+++ Assets/Assets/Script/Player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using System;
+using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
@@ -12,11 +13,17 @@ public class Player : MonoBehaviour
     public bool yaVolo, estaVivo;
     public float valorOffset = 0;
 
-    private float velocidad = 3.0f, fuerzarebote = 4.0f, velocidadAngulo = 15; // aqui podria cambiar los valores si quiero agregar difficultad maybe
+    private float velocidad = 4.0f, fuerzarebote = 4.0f, velocidadAngulo = 50; // aqui podria cambiar los valores si quiero agregar difficultad maybe
     private Button btnvolar;
+
+    private int score;
+    public AudioSource reproductor;
+    public AudioClip sndPunto, sndMuere, sndVuelo;
+    public Text txtScore;
 
     void Awake()
     {
+        score = 0;
         if (instancia == null)
         {
             instancia = this;
@@ -41,6 +48,8 @@ public class Player : MonoBehaviour
                 yaVolo = false;
                 rb2d.linearVelocity = new Vector2(0, fuerzarebote);
                 anim.SetTrigger("volando");
+                reproductor.clip = sndVuelo;
+                reproductor.Play();
             }
 
             if (rb2d.linearVelocity.y >= 0)
@@ -70,5 +79,31 @@ public class Player : MonoBehaviour
     private void VuelaPajaro()
     {
         yaVolo = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D obj)
+    {
+        if (obj.tag == "tuboGrupo")
+        {
+            score++;
+            txtScore.text = score.ToString();
+            reproductor.clip = sndPunto;
+            reproductor.Play();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "piso" || obj.gameObject.tag == "tubo")
+        {
+            if (estaVivo)
+            {
+                estaVivo = false;
+                anim.SetTrigger("muere");
+                reproductor.clip = sndMuere;
+                reproductor.Play();
+
+            }
+        }
     }
 }
